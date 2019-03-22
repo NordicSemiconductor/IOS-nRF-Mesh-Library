776885c97c22d6c89a4ac258766d1ea3aec8c55b
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2019-03-22 15:40:32 +0100

    Missing files (still does not compile)

---
+++ Example/nRFMeshProvision/View Controllers/Settings/ProvisionersViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ProvisionersViewController.swift
+//  nRFMeshProvision_Example
+//
+//  Created by Aleksander Nowakowski on 18/03/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import nRFMeshProvision
+
+class ProvisionersViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if !self.navigationItem.rightBarButtonItems!.contains(self.editButtonItem) {
+            self.navigationItem.rightBarButtonItems!.append(self.editButtonItem)
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Provisioners"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MeshNetworkManager.instance.meshNetwork?.provisioners.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "provisionerCell", for: indexPath)
+
+        let provisioner = MeshNetworkManager.instance.meshNetwork!.provisioners[indexPath.count]
+        cell.textLabel?.text = provisioner.provisionerName
+        cell.detailTextLabel?.text = provisioner.uuid.uuidString
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return MeshNetworkManager.instance.meshNetwork?.provisioners.count ?? 0 > 1
+    }
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+}
