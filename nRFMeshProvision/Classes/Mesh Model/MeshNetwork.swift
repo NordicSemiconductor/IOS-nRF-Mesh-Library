2ed43f3cb271e8a8fd4828e460b1bb92204448c3
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2019-04-01 10:52:59 +0200

    Small refactoring

--- nRFMeshProvision/Classes/Mesh Model/MeshNetwork.swift
+++ nRFMeshProvision/Classes/Mesh Model/MeshNetwork.swift
@@ -137,13 +137,13 @@ extension MeshNetwork {
     /// The 0'th element is identified by the node's unicast address.
     /// Each following element is identified by a subsequent unicast address.
     ///
-    /// - parameter elements:    The number of node's elements. Each element will be
+    /// - parameter count:       The number of node's elements. Each element will be
     ///                          identified by a subsequent unicast address.
     /// - parameter provisioner: The provisioner that is creating the node.
     ///                          The address will be taken from it's allocated range.
     /// - returns: The next available unicast address that can be assigned to a node,
     ///            or nil, if there are no more available addresses in the allocated range.
-    func allocateNextAvailableUnicastAddress(for elements: UInt16, elementsUsing provisioner: Provisioner) -> Address? {
+    func allocateNextAvailableUnicastAddress(for count: UInt16, elementsUsing provisioner: Provisioner) -> Address? {
         let sortedNodes = nodes.sorted { $0.unicastAddress < $1.unicastAddress }
         
         // Iterate through all nodes just once, while iterating over ranges.
@@ -163,20 +163,20 @@ extension MeshNetwork {
                     continue
                 }
                 // If we found a space before the current node, return the address.
-                if address + elements < node.unicastAddress {
+                if address + count < node.unicastAddress {
                     return address
                 }
                 // Else, mode the address to the end of the node's assigned addersses.
                 address = node.unicastAddress + node.elementsCount
                 
                 // If the new address is outside of the range, go to the next one.
-                if address + elements > range.highAddress {
+                if address + count > range.highAddress {
                     break
                 }
             }
             
             // If the range has available space, return the address.
-            if address + elements <= range.highAddress {
+            if address + count <= range.highAddress {
                 return address
             }
         }
