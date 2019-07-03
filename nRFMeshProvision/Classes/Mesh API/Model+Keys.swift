d55edc50191efa58d98099911f0318dedc3c4072
philips77 <aleksander.nowakowski@nordicsemi.no>
2019-07-03 13:37:30 +0200

    Comment

--- nRFMeshProvision/Classes/Mesh API/Model+Keys.swift
+++ nRFMeshProvision/Classes/Mesh API/Model+Keys.swift
@@ -10,6 +10,9 @@ import Foundation
 public extension Model {
     
     /// List of Application Keys bound to this Model.
+    ///
+    /// The list will not contain unknown Application Keys bound
+    /// to this Model, possibly bound by other Provisioner.
     var boundApplicationKeys: [ApplicationKey] {
         return parentElement?.parentNode?.applicationKeys.filter({ bind.contains($0.index) }) ?? []
     }
