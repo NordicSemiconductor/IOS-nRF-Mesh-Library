093b06be53d90704f8d26d11258ca540b777f1e1
philips77 <aleksander.nowakowski@nordicsemi.no>
2019-07-03 11:49:53 +0200

    Binding App Keys to Models

---
+++ Example/nRFMeshProvision/View Controllers/Network/Configuration/ModelBindAppKeyViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ModelBindAppKeyViewController.swift
+//  nRFMeshProvision_Example
+//
+//  Created by Aleksander Nowakowski on 02/07/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import nRFMeshProvision
+
+protocol BindAppKeyDelegate {
+    /// This method is called when a new Application Key has been bound to the Model.
+    func keyBound()
+}
+
+class ModelBindAppKeyViewController: ConnectableViewController {
+    
+    // MARK: - Outlets and Actions
+    
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+        guard let selectedIndexPath = selectedIndexPath else {
+            return
+        }
+        let selectedAppKey = keys[selectedIndexPath.row]
+        whenConnected() { alert in
+            guard let node = self.model.parentElement.parentNode else {
+                self.done()
+                return
+            }
+            alert?.message = "Binding Application Key..."
+            MeshNetworkManager.instance.send(ConfigModelAppBind(applicationKey: selectedAppKey, to: self.model), to: node)
+        }
+    }
+    
+    // MARK: - Properties
+    
+    var model: Model!
+    var delegate: BindAppKeyDelegate?
+    
+    private var keys: [ApplicationKey]!
+    private var selectedIndexPath: IndexPath?
+    
+    // MARK: - View Controller
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.setEmptyView(title: "No keys available", message: "Add a new key to the node first.", messageImage: #imageLiteral(resourceName: "baseline-key"))
+        
+        MeshNetworkManager.instance.delegate = self
+        
+        keys = model.parentElement?.parentNode?.applicationKeysAvailableFor(model)
+        if keys.isEmpty {
+            tableView.showEmptyView()
+        }
+        // Initially, no key is checked.
+        doneButton.isEnabled = false
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return keys.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let key = keys[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = key.name
+        cell.detailTextLabel?.text = "Bound to \(key.boundNetworkKey.name)"
+        cell.accessoryType = indexPath == selectedIndexPath ? .checkmark : .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        var rows: [IndexPath] = []
+        if let selectedIndexPath = selectedIndexPath {
+            rows.append(selectedIndexPath)
+        }
+        rows.append(indexPath)
+        selectedIndexPath = indexPath
+        tableView.reloadRows(at: rows, with: .automatic)
+        
+        doneButton.isEnabled = true
+    }
+
+}
+
+extension ModelBindAppKeyViewController: MeshNetworkDelegate {
+    
+    func meshNetwork(_ meshNetwork: MeshNetwork, didDeliverMessage message: MeshMessage, from source: Address) {
+        switch message {
+        case let status as ConfigModelAppStatus:
+            done() {
+                if status.status == .success {
+                    self.dismiss(animated: true)
+                    self.delegate?.keyBound()
+                } else {
+                    self.presentAlert(title: "Error", message: "\(status.status)")
+                }
+            }
+        default:
+            // Ignore
+            break
+        }
+    }
+    
+}
--- Example/nRFMeshProvision/View Controllers/Network/Configuration/ModelViewController.swift
+++ Example/nRFMeshProvision/View Controllers/Network/Configuration/ModelViewController.swift
@@ -23,55 +23,143 @@ class ModelViewController: ConnectableViewController {
         title = model.name ?? "Model"
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "bind" {
+            let destination = segue.destination as! UINavigationController
+            let viewController = destination.topViewController as! ModelBindAppKeyViewController
+            viewController.model = model
+            viewController.delegate = self
+        }
+    }
+    
     // MARK: - Table View Controller
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        if model.isBluetoothSIGAssigned && model.isConfigurationServer {
+            // TODO: Add Relay and Transmit controlls.
+            return 1
+        }
+        if model.isBluetoothSIGAssigned && model.isConfigurationClient {
+            return 1
+        }
+        return 2 // TODO: Add Publish, Subscribe and custom sections
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case IndexPath.detailsSection:
             return IndexPath.detailsTitles.count
+        case IndexPath.bindingsSection:
+            return model.boundApplicationKeys.count + 1 // Add Action.
         default:
             return 0
         }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //if indexPath.isDetailsSection {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "normal", for: indexPath)
-        cell.textLabel?.text = indexPath.title
-        cell.selectionStyle = .none
-        if indexPath.isModelId {
-            cell.detailTextLabel?.text = model.modelIdentifier.asString()
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case IndexPath.bindingsSection:
+            return "Bound Application Keys"
+        case IndexPath.publishSection:
+            return "Publication"
+        case IndexPath.subscribeSection:
+            return "Subscriptions"
+        default:
+            return nil
         }
-        if indexPath.isCompany {
-            if model.isBluetoothSIGAssigned {
-                cell.detailTextLabel?.text = "Bluetooth SIG"
-            } else {
-                if let companyId = model.companyIdentifier {
-                    if let companyName = CompanyIdentifier.name(for: companyId) {
-                        cell.detailTextLabel?.text = companyName
-                    } else {
-                        cell.detailTextLabel?.text = "Unknown Company ID (\(companyId.asString()))"
-                    }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if indexPath.isBindingsSection {
+            return indexPath.row < model.boundApplicationKeys.count
+        }
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.isDetailsSection {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "normal", for: indexPath)
+            cell.textLabel?.text = indexPath.title
+            if indexPath.isModelId {
+                cell.detailTextLabel?.text = model.modelIdentifier.asString()
+            }
+            if indexPath.isCompany {
+                if model.isBluetoothSIGAssigned {
+                    cell.detailTextLabel?.text = "Bluetooth SIG"
                 } else {
-                    cell.detailTextLabel?.text = "Unknown Company ID"
+                    if let companyId = model.companyIdentifier {
+                        if let companyName = CompanyIdentifier.name(for: companyId) {
+                            cell.detailTextLabel?.text = companyName
+                        } else {
+                            cell.detailTextLabel?.text = "Unknown Company ID (\(companyId.asString()))"
+                        }
+                    } else {
+                        cell.detailTextLabel?.text = "Unknown Company ID"
+                    }
                 }
             }
+            return cell
         }
-        return cell
-        //}
+        if indexPath.isBindingsSection {
+            guard indexPath.row < model.boundApplicationKeys.count else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "action", for: indexPath)
+                cell.textLabel?.text = "Bind Application Key"
+                return cell
+            }
+            let applicationKey = model.boundApplicationKeys[indexPath.row]
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "subtitle", for: indexPath)
+            cell.textLabel?.text = applicationKey.name
+            cell.detailTextLabel?.text = "Bound to \(applicationKey.boundNetworkKey.name)"
+            return cell
+        }
+        // Not possible.
+        return tableView.dequeueReusableCell(withIdentifier: "normal", for: indexPath)
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        if indexPath.isBindingsSection {
+            return indexPath.row == model.boundApplicationKeys.count
+        }
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.isBindingsSection {
+            // Only the "Bind" row is selectable.
+            performSegue(withIdentifier: "bind", sender: indexPath)
+        }
     }
 
+}
+
+extension ModelViewController: BindAppKeyDelegate {
+    
+    func keyBound() {
+        tableView.reloadData()
+    }
+    
+}
+
+private extension Model {
+    
+    var isConfigurationServer: Bool {
+        return isBluetoothSIGAssigned && modelIdentifier == 0x0000
+    }
+    
+    var isConfigurationClient: Bool {
+        return isBluetoothSIGAssigned && modelIdentifier == 0x0001
+    }
+    
 }
 
 private extension IndexPath {
     static let detailsSection   = 0
     static let bindingsSection  = 1
     static let publishSection   = 2
-    static let sunscribeSection = 3
+    static let subscribeSection = 3
     
     static let detailsTitles = [
         "Model ID", "Company"
@@ -84,15 +172,19 @@ private extension IndexPath {
         return nil
     }
     
-    var isDetailsSection: Bool {
-        return section == IndexPath.detailsSection
-    }
-    
     var isModelId: Bool {
         return isDetailsSection && row == 0
     }
     
     var isCompany: Bool {
         return isDetailsSection && row == 1
+    }
+    
+    var isDetailsSection: Bool {
+        return section == IndexPath.detailsSection
+    }
+    
+    var isBindingsSection: Bool {
+        return section == IndexPath.bindingsSection
     }
 }
