f9b6dd88b95b1ed8a6e9d05eaf4fcc18ec93e3bc
philips77 <aleksander.nowakowski@nordicsemi.no>
2019-11-20 13:54:47 +0100

    Scenes messages added

--- nRFMeshProvision/Classes/Mesh Messages/GenericMessage.swift
+++ nRFMeshProvision/Classes/Mesh Messages/GenericMessage.swift
@@ -33,9 +33,24 @@ import Foundation
 public protocol GenericMessage: StaticMeshMessage {
     // No additional fields.
 }
+
 public protocol AcknowledgedGenericMessage: GenericMessage, StaticAcknowledgedMeshMessage {
     // No additional fields.
 }
+
+public extension Array where Element == GenericMessage.Type {
+    
+    /// A helper method that can create a map of message types required
+    /// by the `ModelDelegate` from a list of `GenericMessage`s.
+    ///
+    /// - returns: A map of message types.
+    func toMap() -> [UInt32 : MeshMessage.Type] {
+        return (self as [StaticMeshMessage.Type]).toMap()
+    }
+    
+}
+
+// MARK: - GenericMessageStatus
 
 public enum GenericMessageStatus: UInt8 {
     case success           = 0x00
@@ -60,18 +75,6 @@ public extension GenericStatusMessage {
     
 }
 
-public extension Array where Element == GenericMessage.Type {
-    
-    /// A helper method that can create a map of message types required
-    /// by the `ModelDelegate` from a list of `GenericMessage`s.
-    ///
-    /// - returns: A map of message types.
-    func toMap() -> [UInt32 : MeshMessage.Type] {
-        return (self as [StaticMeshMessage.Type]).toMap()
-    }
-    
-}
-
 extension GenericMessageStatus: CustomDebugStringConvertible {
     
     public var debugDescription: String {
@@ -82,6 +85,46 @@ extension GenericMessageStatus: CustomDebugStringConvertible {
             return "Cannot Set Range Min"
         case .cannotSetRangeMax:
             return "Cannot Set Range Max"
+        }
+    }
+    
+}
+
+// MARK: - SceneMessageStatus
+
+public enum SceneMessageStatus: UInt8 {
+    case success           = 0x00
+    case sceneRegisterFull = 0x01
+    case sceneNotFound     = 0x02
+}
+
+public protocol SceneStatusMessage: GenericMessage, StatusMessage {
+    /// Operation status.
+    var status: SceneMessageStatus { get }
+}
+
+public extension SceneStatusMessage {
+    
+    var isSuccess: Bool {
+        return status == .success
+    }
+    
+    var message: String {
+        return "\(status)"
+    }
+    
+}
+
+extension SceneMessageStatus: CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        switch self {
+        case .success:
+            return "Success"
+        case .sceneRegisterFull:
+            return "Scene Register Full"
+        case .sceneNotFound:
+            return "Scene Not Found"
         }
     }
     
---
+++ nRFMeshProvision/Classes/Mesh Messages/Time and Scenes/SceneRecallUnacknowledged.swift
@@ -0,0 +1,89 @@
+/*
+* Copyright (c) 2019, Nordic Semiconductor
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice, this
+*    list of conditions and the following disclaimer in the documentation and/or
+*    other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+* NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import Foundation
+
+public struct SceneRecallUnacknowledged: GenericMessage, TransactionMessage, TransitionMessage {
+    public static let opCode: UInt32 = 0x8243
+    
+    public var tid: UInt8!
+    public var parameters: Data? {
+        let data = Data() + scene + tid
+        if let transitionTime = transitionTime, let delay = delay {
+            return data + transitionTime.rawValue + delay
+        } else {
+            return data
+        }
+    }
+    
+    /// The number of the scene to be recalled.
+    public let scene: Scene
+    
+    public let transitionTime: TransitionTime?
+    public let delay: UInt8?
+    
+    /// Creates the Scene Recall message.
+    ///
+    /// - parameter scene: The number of the scene to be recalled.
+    public init(_ scene: Scene) {
+        self.scene = scene
+        self.transitionTime = nil
+        self.delay = nil
+    }
+    
+    /// Creates the Scene Recall Unacknowledged message.
+    ///
+    /// - parameters:
+    ///   - scene: The number of the scene to be recalled.
+    ///   - transitionTime: The time that an element will take to transition
+    ///                     to the target state from the present state.
+    ///   - delay: Message execution delay in 5 millisecond steps.
+    public init(_ scene: Scene, transitionTime: TransitionTime, delay: UInt8) {
+        self.scene = scene
+        self.transitionTime = transitionTime
+        self.delay = delay
+    }
+    
+    public init?(parameters: Data) {
+        guard parameters.count == 3 || parameters.count == 5 else {
+            return nil
+        }
+        scene = parameters.read()
+        tid = parameters[2]
+        if parameters.count == 5 {
+            transitionTime = TransitionTime(rawValue: parameters[3])
+            delay = parameters[4]
+        } else {
+            transitionTime = nil
+            delay = nil
+        }
+    }
+    
+}
