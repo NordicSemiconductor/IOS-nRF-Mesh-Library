ad4ef902cba6a379aef4c5c646e0d4f2268c4cd5
philips77 <aleksander.nowakowski@nordicsemi.no>
2021-02-10 12:39:39 +0100

    Crypto tests fixed

--- Example/Tests/CryptoTest.swift
+++ Example/Tests/CryptoTest.swift
@@ -43,82 +43,75 @@ class CryptoTest: XCTestCase {
         XCTAssertEqual(random.count, 16)
     }
 
-    func testCalculateCMAC() throws {
-        let expected = Data(hex: "55751E8031296352DE905E2450F1552A")
+    func testVirtualLabel() throws {
+        let label = UUID(uuidString: "12345678-1234-1234-1234-12345678ABCD")!
+        let expected = Address(0xADD5)
         
-        let result = Crypto.calculateCMAC(data, andKey: key)
+        let result = Crypto.calculateVirtualAddress(from: label)
         XCTAssertEqual(result, expected)
     }
     
-    func testCalculateSalt() throws {
-        let source   = "test".data(using: .utf8)!
-        let expected = Data(hex: "b73cefbd641ef2ea598c2b6efb62f79c")
-        
-        let result = Crypto.calculateSalt(source)
-        XCTAssertEqual(result, expected)
-    }
-    
-    func testCCM_MIC4() throws {
+    func testEncryption_MIC4() throws {
         let expected = Data(hex: "6C7854C1E573CD62155BFA987C70673D273AB343")
         
         // Encode
-        let result = Crypto.calculateCCM(data, withKey: key, nonce: nonce, andMICSize: 4,
-                                         withAdditionalData: nil)
+        let result = Crypto.encrypt(data, withEncryptionKey: key, nonce: nonce, andMICSize: 4,
+                                    withAdditionalData: nil)
         XCTAssertEqual(result, expected)
         
         // Decode
         let encrypted = result.subdata(in: 0..<data.count)
         let mic       = result.subdata(in: data.count..<result.count)
-        let test = Crypto.calculateDecryptedCCM(encrypted, withKey: key, nonce: nonce,
-                                                andMIC: mic, withAdditionalData: nil)
+        let test = Crypto.decrypt(encrypted, withEncryptionKey: key, nonce: nonce,
+                                  andMIC: mic, withAdditionalData: nil)
         XCTAssertEqual(test, data)
     }
     
-    func testCCM_MIC8() throws {
+    func testEncryption_MIC8() throws {
         let expected = Data(hex: "6C7854C1E573CD62155BFA987C70673D5CFCB5AC7E3CEA62")
         
         // Encode
-        let result = Crypto.calculateCCM(data, withKey: key, nonce: nonce, andMICSize: 8,
-                                         withAdditionalData: nil)
+        let result = Crypto.encrypt(data, withEncryptionKey: key, nonce: nonce, andMICSize: 8,
+                                    withAdditionalData: nil)
         XCTAssertEqual(result, expected)
         
         // Decode
         let encrypted = result.subdata(in: 0..<data.count)
         let mic       = result.subdata(in: data.count..<result.count)
-        let test = Crypto.calculateDecryptedCCM(encrypted, withKey: key, nonce: nonce,
-                                                andMIC: mic, withAdditionalData: nil)
+        let test = Crypto.decrypt(encrypted, withEncryptionKey: key, nonce: nonce,
+                                  andMIC: mic, withAdditionalData: nil)
         XCTAssertEqual(test, data)
     }
     
-    func testCCM_MIC4_withAdditionalData() throws {
+    func testEncryption_MIC4_withAdditionalData() throws {
         let expected = Data(hex: "6C7854C1E573CD62155BFA987C70673D19F0C64D")
         
         // Encode
-        let result = Crypto.calculateCCM(data, withKey: key, nonce: nonce, andMICSize: 4,
-                                         withAdditionalData: aad)
+        let result = Crypto.encrypt(data, withEncryptionKey: key, nonce: nonce, andMICSize: 4,
+                                    withAdditionalData: aad)
         XCTAssertEqual(result, expected)
         
         // Decode
         let encrypted = result.subdata(in: 0..<data.count)
         let mic       = result.subdata(in: data.count..<result.count)
-        let test = Crypto.calculateDecryptedCCM(encrypted, withKey: key, nonce: nonce,
-                                                andMIC: mic, withAdditionalData: aad)
+        let test = Crypto.decrypt(encrypted, withEncryptionKey: key, nonce: nonce,
+                                  andMIC: mic, withAdditionalData: aad)
         XCTAssertEqual(test, data)
     }
     
-    func testCCM_MIC8_withAdditionalData() throws {
+    func testEncryption_MIC8_withAdditionalData() throws {
         let expected = Data(hex: "6C7854C1E573CD62155BFA987C70673D37D0CC6CAEF67CFC")
         
         // Encode
-        let result = Crypto.calculateCCM(data, withKey: key, nonce: nonce, andMICSize: 8,
-                                         withAdditionalData: aad)
+        let result = Crypto.encrypt(data, withEncryptionKey: key, nonce: nonce, andMICSize: 8,
+                                    withAdditionalData: aad)
         XCTAssertEqual(result, expected)
         
         // Decode
         let encrypted = result.subdata(in: 0..<data.count)
         let mic       = result.subdata(in: data.count..<result.count)
-        let test = Crypto.calculateDecryptedCCM(encrypted, withKey: key, nonce: nonce,
-                                                andMIC: mic, withAdditionalData: aad)
+        let test = Crypto.decrypt(encrypted, withEncryptionKey: key, nonce: nonce,
+                                  andMIC: mic, withAdditionalData: aad)
         XCTAssertEqual(test, data)
     }
     
@@ -139,46 +132,38 @@ class CryptoTest: XCTestCase {
         XCTAssertEqual(deobfuscated, source)
     }
     
-    func testK1() throws {
-        let N    = Data(hex: "3216d1509884b533248541792b877f98")
-        let SALT = Data(hex: "2ba14ffa0df84a2831938d57d276cab4")
-        let P    = Data(hex: "5a09d60797eeb4478aada59db3352a0d")
-        
-        let expected = Data(hex: "f6ed15a8934afbe7d83e8dcb57fcf5d7")
-        
-        let k1 = Crypto.calculateK1(withN: N, salt: SALT, andP: P)
-        XCTAssertEqual(k1, expected)
-    }
-    
-    func testK2() throws {
-        let N = Data(hex: "f7a2a44f8e8a8029064f173ddc1e2b00")
-        let P = Data(hex: "00")
+    func testKeyDerivatives() throws {
+        let key = Data(hex: "f7a2a44f8e8a8029064f173ddc1e2b00")
         
         let expectedNID           = UInt8(0x7F)
         let expectedEncryptionKey = Data(hex: "9f589181a0f50de73c8070c7a6d27f46")
         let expectedPrivacyKey    = Data(hex: "4c715bd4a64b938f99b453351653124f")
+        let expectedIdentityKey   = Data(hex: "877DE1A131C87A8C6767E655061963A7")
+        let expectedBeaconKey     = Data(hex: "CCAE3C53A3BB6FAB728EE94A390DC91F")
         
-        let (n, e, p) = Crypto.calculateK2(withN: N, andP: P)
+        let (n, e, p, i, b) = Crypto.calculateKeyDerivatives(from: key)
         XCTAssertEqual(n, expectedNID)
         XCTAssertEqual(e, expectedEncryptionKey)
         XCTAssertEqual(p, expectedPrivacyKey)
+        XCTAssertEqual(i, expectedIdentityKey)
+        XCTAssertEqual(b, expectedBeaconKey)
     }
     
-    func testK3() throws {
-        let N = Data(hex: "f7a2a44f8e8a8029064f173ddc1e2b00")
+    func testNetworkId() throws {
+        let key = Data(hex: "f7a2a44f8e8a8029064f173ddc1e2b00")
         
         let expected = Data(hex: "ff046958233db014")
         
-        let k3 = Crypto.calculateK3(withN: N)
+        let k3 = Crypto.calculateNetworkId(from: key)
         XCTAssertEqual(k3, expected)
     }
     
-    func testK4() throws {
-        let N = Data(hex: "3216d1509884b533248541792b877f98")
+    func testAid() throws {
+        let key = Data(hex: "3216d1509884b533248541792b877f98")
         
         let expectedAID = UInt8(0x38)
         
-        let k4 = Crypto.calculateK4(withN: N)
+        let k4 = Crypto.calculateAid(from: key)
         XCTAssertEqual(k4, expectedAID)
     }
     
