c6beb56e598fd0b1dbead898b2496af6424f9885
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-02-16 21:12:51 +0100

    Light LC Occupancy Mode messages

---
+++ nRFMeshProvision/Classes/Mesh Messages/Lighting/LightLCOccupancyModeSet.swift
@@ -0,0 +1,62 @@
+/*
+* Copyright (c) 2021, Nordic Semiconductor
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice, this
+*    list of conditions and the following disclaimer in the documentation and/or
+*    other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+* NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import Foundation
+
+public struct LightLCOccupancyModeSet: AcknowledgedGenericMessage {
+    public static let opCode: UInt32 = 0x8296
+    public static let responseType: StaticMeshMessage.Type = LightLCOccupancyModeStatus.self
+    
+    /// Whether the controller may transition from a standby state when occupancy
+    /// is reported.
+    let occupancyMode: Bool
+    
+    public var parameters: Data? {
+        return Data() + occupancyMode
+    }
+    
+    /// Creates the Light LC Occupancy Mode Set Unacknowledged message.
+    ///
+    /// - parameter mode: The present value of the Light LC Occupancy Mode state.
+    public init(_ mode: Bool) {
+        self.occupancyMode = status
+    }
+    
+    public init?(parameters: Data) {
+        guard parameters.count == 1 else {
+            return nil
+        }
+        guard parameters[0] == 0 || parameters[0] == 1 else {
+            return nil
+        }
+        self.occupancyMode == parameters[0] == 1
+    }
+    
+}
