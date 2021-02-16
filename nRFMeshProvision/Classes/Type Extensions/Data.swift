203cb5e3f0b2aa57d538a10d3dd36a74668f4c61
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-02-16 20:59:44 +0100

    Bool as DataConvertible

--- nRFMeshProvision/Classes/Type Extensions/Data.swift
+++ nRFMeshProvision/Classes/Type Extensions/Data.swift
@@ -128,3 +128,15 @@ extension Data : DataConvertible {
     }
     
 }
+
+extension Bool : DataConvertible {
+    
+    static func + (lhs: Data, rhs: Bool) -> Data {
+        if rhs {
+            return lhs + UInt8(0x01)
+        } else {
+            return lhs + UInt8(0x00)
+        }
+    }
+    
+}
