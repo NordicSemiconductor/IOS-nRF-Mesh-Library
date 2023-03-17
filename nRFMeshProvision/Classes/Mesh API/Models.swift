0e963297f54a2d78a69e98d990d5da3786f7d1d6
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-03-17 16:20:17 +0100

    Support for model publication and subsctiption

--- nRFMeshProvision/Classes/Mesh API/Models.swift
+++ nRFMeshProvision/Classes/Mesh API/Models.swift
@@ -50,4 +50,156 @@ public extension Model {
         return subscriptions.contains { $0.address == address }
     }
     
+    /// Whether the Model supports model publication defined in Section 4.2.3 in
+    /// Bluetooth Mesh Profile 1.0.1 specification.
+    ///
+    /// - returns: `true` if the model supports model publication, `false` ir it doesn't
+    ///            or `nil` if unknown.
+    var supportsModelPublication: Bool? {
+        if !isBluetoothSIGAssigned {
+            return nil
+        }
+        switch modelIdentifier {
+        // Foundation
+        case 0x0000: return false // Configuration Server
+        case 0x0001: return false // Configuration Client
+        case 0x0002: return true  // Health Server
+        case 0x0003: return true  // Health Client
+        // Generic
+        case 0x1000: return true  // Generic OnOff Server
+        case 0x1001: return true  // Generic OnOff Client
+        case 0x1002: return true  // Generic Level Server
+        case 0x1003: return true  // Generic Level Client
+        case 0x1004: return true  // Generic Default Transition Time Server
+        case 0x1005: return true  // Generic Default Transition Time Client
+        case 0x1006: return true  // Generic Power OnOff Server
+        case 0x1007: return false // Generic Power OnOff Setup Server = only subsc
+        case 0x1008: return true  // Generic Power OnOff Client
+        case 0x1009: return true  // Generic Power Level Server
+        case 0x100A: return false // Generic Power Level Setup Server
+        case 0x100B: return true  // Generic Power Level Client
+        case 0x100C: return true  // Generic Battery Server
+        case 0x100D: return true  // Generic Battery Client
+        case 0x100E: return true  // Generic Location Server
+        case 0x100F: return false // Generic Location Setup Server
+        case 0x1010: return true  // Generic Location Client
+        case 0x1011: return true  // Generic Admin Property Server
+        case 0x1012: return true  // Generic Manufacturer Property Server
+        case 0x1013: return true  // Generic User Property Server
+        case 0x1014: return true  // Generic Client Property Server
+        case 0x1015: return true  // Generic Property Client
+        // Sensors
+        case 0x1100: return true  // Sensor Server
+        case 0x1101: return true  // Sensor Setup Server
+        case 0x1102: return true  // Sensor Client
+        // Time and Scenes
+        case 0x1200: return true  // Time Server
+        case 0x1201: return false // Time Setup Server
+        case 0x1202: return true  // Time Client
+        case 0x1203: return true  // Scene Server
+        case 0x1204: return false // Scene Setup Server
+        case 0x1205: return true  // Scene Client
+        case 0x1206: return true  // Scheduler Server
+        case 0x1207: return false // Scheduler Setup Server
+        case 0x1208: return true  // Scheduler Client
+        // Lighting
+        case 0x1300: return true  // Light Lightness Server
+        case 0x1301: return false // Light Lightness Setup Server
+        case 0x1302: return true  // Light Lightness Client
+        case 0x1303: return true  // Light CTL Server
+        case 0x1304: return false // Light CTL Setup Server
+        case 0x1305: return true  // Light CTL Client
+        case 0x1306: return true  // Light CTL Temperature Server
+        case 0x1307: return true  // Light HSL Server
+        case 0x1308: return false // Light HSL Setup Server
+        case 0x1309: return true  // Light HSL Client
+        case 0x130A: return true  // Light HSL Hue Server
+        case 0x130B: return true  // Light HSL Saturation Server
+        case 0x130C: return true  // Light xyL Server
+        case 0x130D: return false // Light xyL Setup Server
+        case 0x130E: return true  // Light xyL Client
+        case 0x130F: return true  // Light LC Server
+        case 0x1310: return true  // Light LC Setup Server
+        case 0x1311: return true  // Light LC Client
+            
+        default: return nil
+        }
+    }
+    
+    /// Whether the Model supports model subscription defined in Section 4.2.4 in
+    /// Bluetooth Mesh Profile 1.0.1 specification.
+    ///
+    /// - returns: `true` if the model supports model subscription, `false` ir it doesn't
+    ///            or `nil` if unknown.
+    var supportsModelSubscriptions: Bool? {
+        if !isBluetoothSIGAssigned {
+            return nil
+        }
+        switch modelIdentifier {
+        // Foundation
+        case 0x0000: return false // Configuration Server
+        case 0x0001: return false // Configuration Client
+        case 0x0002: return true  // Health Server
+        case 0x0003: return true  // Health Client
+        // Generic
+        case 0x1000: return true  // Generic OnOff Server
+        case 0x1001: return true  // Generic OnOff Client
+        case 0x1002: return true  // Generic Level Server
+        case 0x1003: return true  // Generic Level Client
+        case 0x1004: return true  // Generic Default Transition Time Server
+        case 0x1005: return true  // Generic Default Transition Time Client
+        case 0x1006: return true  // Generic Power OnOff Server
+        case 0x1007: return true  // Generic Power OnOff Setup Server
+        case 0x1008: return true  // Generic Power OnOff Client
+        case 0x1009: return true  // Generic Power Level Server
+        case 0x100A: return true  // Generic Power Level Setup Server
+        case 0x100B: return true  // Generic Power Level Client
+        case 0x100C: return true  // Generic Battery Server
+        case 0x100D: return true  // Generic Battery Client
+        case 0x100E: return true  // Generic Location Server
+        case 0x100F: return true  // Generic Location Setup Server
+        case 0x1010: return true  // Generic Location Client
+        case 0x1011: return true  // Generic Admin Property Server
+        case 0x1012: return true  // Generic Manufacturer Property Server
+        case 0x1013: return true  // Generic User Property Server
+        case 0x1014: return true  // Generic Client Property Server
+        case 0x1015: return true  // Generic Property Client
+        // Sensors
+        case 0x1100: return true  // Sensor Server
+        case 0x1101: return true  // Sensor Setup Server
+        case 0x1102: return true  // Sensor Client
+        // Time and Scenes
+        case 0x1200: return true  // Time Server
+        case 0x1201: return false // Time Setup Server
+        case 0x1202: return true  // Time Client
+        case 0x1203: return true  // Scene Server
+        case 0x1204: return true  // Scene Setup Server
+        case 0x1205: return true  // Scene Client
+        case 0x1206: return true  // Scheduler Server
+        case 0x1207: return true  // Scheduler Setup Server
+        case 0x1208: return true  // Scheduler Client
+        // Lighting
+        case 0x1300: return true  // Light Lightness Server
+        case 0x1301: return true  // Light Lightness Setup Server
+        case 0x1302: return true  // Light Lightness Client
+        case 0x1303: return true  // Light CTL Server
+        case 0x1304: return true  // Light CTL Setup Server
+        case 0x1305: return true  // Light CTL Client
+        case 0x1306: return true  // Light CTL Temperature Server
+        case 0x1307: return true  // Light HSL Server
+        case 0x1308: return true  // Light HSL Setup Server
+        case 0x1309: return true  // Light HSL Client
+        case 0x130A: return true  // Light HSL Hue Server
+        case 0x130B: return true  // Light HSL Saturation Server
+        case 0x130C: return true  // Light xyL Server
+        case 0x130D: return true  // Light xyL Setup Server
+        case 0x130E: return true  // Light xyL Client
+        case 0x130F: return true  // Light LC Server
+        case 0x1310: return true  // Light LC Setup Server
+        case 0x1311: return true  // Light LC Client
+            
+        default: return nil
+        }
+    }
+    
 }
