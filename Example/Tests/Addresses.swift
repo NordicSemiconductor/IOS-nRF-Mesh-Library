ba2785962d2912935a4f189f59c3c4ca8ca8f898
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-03-23 14:17:10 +0100

    Unused methods removed

--- Example/Tests/Addresses.swift
+++ Example/Tests/Addresses.swift
@@ -32,27 +32,12 @@ import XCTest
 @testable import nRFMeshProvision
 
 class Addresses: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     func testVirtualAddress() {
         let virtualLabel = UUID(uuidString: "0073e7e4-d8b9-440f-af84-15df4c56c0e1")!
         let address = MeshAddress(virtualLabel)
         
         XCTAssertEqual(address.address, 0xB529)
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
