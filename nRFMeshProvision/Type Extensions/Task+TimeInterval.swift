6aaefaa35c4f66d1955d782c05e6260de3346081
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-09-19 15:47:52 +0200

    A helper method to sleep for number of seconds

--- nRFMeshProvision/Type Extensions/Task+TimeInterval.swift
+++ nRFMeshProvision/Type Extensions/Task+TimeInterval.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+internal extension Task where Success == Never, Failure == Never {
+    
+    static func sleep(seconds: TimeInterval) async throws {
+        let duration = UInt64(seconds * 1_000_000_000)
+        try await Task.sleep(nanoseconds: duration)
+    }
+    
+}
