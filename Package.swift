b42b741d6a9c66cf513f8f7375eda0602de8ecf0
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-10-10 17:35:41 +0200

    CryptoSwfit updated to 1.8.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/krzyzanowskim/CryptoSwift", 
-      .upToNextMinor(from: "1.7.0")
+      .upToNextMinor(from: "1.8.0")
     ),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
