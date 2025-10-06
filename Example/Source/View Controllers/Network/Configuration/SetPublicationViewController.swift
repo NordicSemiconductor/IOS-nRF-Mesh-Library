70895009846122f93be1d534e2921321c116620e
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2025-10-06 13:31:55 +0200

    Row height fixed for setting Friendship Credentials Flag row

--- Example/Source/View Controllers/Network/Configuration/SetPublicationViewController.swift
+++ Example/Source/View Controllers/Network/Configuration/SetPublicationViewController.swift
@@ -159,9 +159,6 @@ class SetPublicationViewController: ProgressViewController {
         if indexPath.isDestination {
             return 123
         }
-        if indexPath.isDetailsSection {
-            return 44
-        }
         return UITableView.automaticDimension
     }
     
